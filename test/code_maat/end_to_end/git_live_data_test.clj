8a6970c4342dd88aeaa0780625415d2207a9ded3
Adam Petersen <adam@adampetersen.se>
2013-08-30 16:12:42 +0200

    git: added an end to end test on live data from mono

---
+++ test/code_maat/end_to_end/git_live_data_test.clj
@@ -0,0 +1,18 @@
+;;; Copyright (C) 2013 Adam Tornhill
+;;;
+;;; Distributed under the GNU General Public License v3.0,
+;;; see http://www.gnu.org/licenses/gpl.html
+
+(ns code-maat.end-to-end.git-live-data-test
+  (:require [code-maat.app.app :as app])
+  (:use clojure.test))
+
+(def ^:const git-log-file "./test/code_maat/end_to_end/mono_git.log")
+
+(deftest parses-live-data
+  (is (= (with-out-str
+           (app/run git-log-file
+                    {:version-control "git"
+                     :analysis "authors"
+                     :rows 2}))
+         "entity,n-authors,n-revs\nmono/metadata/sgen-gc.c,2,2\nmcs/class/corlib/System/Console.iOS.cs,2,3\n")))
