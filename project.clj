bda51b56ddf0fb66b268af63424c25f944703da7
Adam Petersen <adam@adampetersen.se>
2013-09-03 06:52:42 +0200

    Documented version 0.2

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0-alpha5"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.zip "0.1.0"]
 		 [incanter "1.5.2"]
                  [org.clojure/tools.cli "0.2.4"]
