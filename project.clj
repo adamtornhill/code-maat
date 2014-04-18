dd8e6057a85f463c8e16cf18c4c1695c9ea34237
Adam Petersen <adam@adampetersen.se>
2014-04-18 15:30:17 +0200

    Library dependencies updated

--- project.clj
+++ project.clj
@@ -3,20 +3,20 @@
 ;;; Distributed under the GNU General Public License v3.0,
 ;;; see http://www.gnu.org/licenses/gpl.html
 
-(defproject code-maat "0.5.0-SNAPSHOT"
+(defproject code-maat "0.5.1"
   :description "A toolset to mine and analyze version control data"
   :url "http://www.adamtornhill.com/code/codemaat.htm"
   :license {:name "GNU General Public License v3.0"
             :url "http://www.gnu.org/licenses/gpl.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/data.zip "0.1.0"]
-		 [incanter "1.5.2"]
-                 [org.clojure/tools.cli "0.2.4"]
+                 [org.clojure/data.zip "0.1.1"]
+		 [incanter "1.5.4"]
+                 [org.clojure/tools.cli "0.3.1"]
                  [org.clojure/data.csv "0.1.2"]
-                 [clj-time "0.5.1"]
-                 [org.clojure/math.numeric-tower "0.0.2"]
-                 [org.clojure/math.combinatorics "0.0.4"]
-                 [instaparse "1.2.2"]]
+                 [clj-time "0.7.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [org.clojure/math.combinatorics "0.0.7"]
+                 [instaparse "1.3.1"]]
   :main code-maat.core
   :aot [code-maat.core]
   :jvm-opts ["-Xmx4g" "-Djava.awt.headless=true"])
