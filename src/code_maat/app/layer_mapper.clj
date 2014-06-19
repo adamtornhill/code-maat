22d14a48aedcaf9c20fd7e4631cac8a51d97dfae
Adam Petersen <adam@adampetersen.se>
2014-06-19 15:15:29 +0200

    New group format => more expressive

--- src/code_maat/app/layer_mapper.clj
+++ src/code_maat/app/layer_mapper.clj
@@ -1,27 +1,101 @@
 ;;; Copyright (C) 2014 Adam Tornhill
 ;;;
 
-(ns code-maat.app.layer-mapper)
+(ns code-maat.app.layer-mapper
+  (:require [instaparse.core :as insta]))
 
 ;;; Code Maat supports analysis according to pre-defined layers.
 ;;; These layers are typically architectural boundaries. All data
 ;;; will be aggregated into that layered view before analysis.
 
-(defn- entity->layer
+;; Parsing the group specification
+;; ===============================
+
+(def ^:const group-grammar
+  "Here's the instaparse grammar for a grouping/layering.
+   We expect the groups to be specified as:
+     some/path => some_name
+   That is, everything on some/path will be grouped
+   under some_name."
+  "
+    <S>       = groups
+    <groups>  = (group <nl*>)* | group
+    group     = path <ws*> <separator> <ws*> name
+    path      = #'^[\\w/\\\\]+'
+    separator = '=>'
+    name      = #'\\w+'
+    ws        =  #'\\s'
+    nl        =  '\\n'")
+
+(defn- raise-parse-failure
+  [f]
+  (let [reason (with-out-str (print f))]
+    (throw (IllegalArgumentException.
+            (str "Invalid group specification: " reason)))))
+
+(defn- as-grammar-map
+  "The actual invokation of the parser.
+   Returns a Hiccup parse tree upon success,
+   otherwise an informative exception is thrown."
+  [parser input]
+  (let [result (insta/parse parser input)]
+    (if (insta/failure? result)
+      (raise-parse-failure (insta/get-failure result))
+      result)))
+
+;;; The parse result from instaparse is given as hiccup vectors.
+;;; We define a set of accessors encapsulating the access to
+;;; the individual parts of the associative vectors.
+
+(defn- as-path
+  [v]
+  (get-in v [1 1]))
+
+(defn- as-name
+  [v]
+  (get-in v [2 1]))
+
+(defn- as-group-specification
+  [v]
+  {:path (as-path v)
+   :name (as-name v)})
+
+(defn text->group-specification
+  "Transforms the given text into a
+   seq of maps specifying the grouping.
+   Each map will have the following content:
+    {:path 'some/path' :name 'some_name'}" 
+  [input]
+  (let [parser (insta/parser group-grammar)]
+    (->>
+     input
+     (as-grammar-map parser)
+     (map as-group-specification))))
+
+;; Mapping physical entities to logical groups
+;; ===========================================
+
+(defn- entity->logical-name
   [entity layer-exprs]
-  (some #(re-find % entity) layer-exprs))
+  (some (fn [{:keys [path-match logical-name]}]
+          (when (re-find path-match entity)
+            logical-name))
+        layer-exprs))
 
 (defn- commit->commit-by-layer
   [commit layer-exprs]
-  (update-in commit [:entity] #(entity->layer % layer-exprs))) 
+  (update-in commit [:entity] #(entity->logical-name % layer-exprs))) 
 
 (defn- within-layers?
   [layer-exprs entity]
-  (some #(re-find % entity) layer-exprs))
+  (->>
+   (map :path-match layer-exprs)
+   (some #(re-find % entity))))
 
 (defn- as-layer-expr
-  [layer]
-  (re-pattern (str "^" layer "/")))
+  [{:keys [path name]}]
+  {:path-match (re-pattern (str "^" path "/"))
+   :logical-name name})
 
 (defn- as-layer-exprs
   [layers]
@@ -30,9 +104,10 @@
 (defn map-entities->layers
   "Maps each entity in the commits to one of the pre-defined
    architectural layers.
-   The layers are given as a seq of strings. Each string denotes the
-   name of a layer. We translate that string into a regex that matches
-   entity names with layer names."
+   The layers are given as a seq of maps. Each map denotes the
+   physical path to the layer together with its logical name.
+   We translate that path into a regex that matches
+   entity names with logical names."
   [commits layers]
   (let [layer-exprs (as-layer-exprs layers)]
     (->>
@@ -41,8 +116,7 @@
 
 (defn- layers-from
   [layer-info-file]
-  (with-open [rdr (clojure.java.io/reader layer-info-file)]
-    (doall (line-seq rdr))))
+  (slurp layer-info-file))
 
 (defn run
   "This entry point parses the given layer info.
@@ -51,4 +125,5 @@
   [layer-info-file commits]
   (->>
    (layers-from layer-info-file)
+   text->group-specification
    (map-entities->layers commits)))
--- test/code_maat/app/layer_mapper_test.clj
+++ test/code_maat/app/layer_mapper_test.clj
@@ -6,6 +6,25 @@
             [incanter.core :as incanter])
   (:use clojure.test))
 
+(def ^:const multi-group-spec
+"/some/path => G1
+/another/path => G2")
+
+(deftest parses-specification
+  (testing "Single group"
+    (is (= (mapper/text->group-specification "/some/path => G1")
+           [{:path "/some/path" :name "G1"}])))
+  (testing "Multiple groups"
+    (is (= (mapper/text->group-specification multi-group-spec)
+           [{:path "/some/path" :name "G1"}
+            {:path "/another/path" :name "G2"}])))
+   (testing "No groups"
+    (is (= (mapper/text->group-specification "")
+           [])))
+   (testing "With backslash"
+    (is (= (mapper/text->group-specification "/some\\path => G1")
+           [{:path "/some\\path" :name "G1"}]))))
+
 (def ^:const entities-in-same-layer [{:entity "Top/A" :rev 1}
                                      {:entity "Top/B" :rev 2}])
 
@@ -14,21 +33,23 @@
 
 (def as-ds incanter/to-dataset)
 
-(def ^:const top-level-layer ["Top"])
-(def ^:const multiple-layers ["Top" "Bottom"])
+;; Specify the path to match a physical layer together with the name
+;; of the logical layer:
+(def ^:const top-level-layer [{:path "Top" :name "T"}])
+(def ^:const multiple-layers [{:path "Top" :name "Top"} {:path "Bottom" :name "infrastructure"}])
 
 (deftest entities-are-mapped-to-defined-layers
   (testing "Mapped to the same layer"
     (is (= (mapper/map-entities->layers entities-in-same-layer top-level-layer)
-           [{:rev 1 :entity "Top/"}
-            {:rev 2 :entity "Top/"}])))
+           [{:rev 1 :entity "T"}
+            {:rev 2 :entity "T"}])))
   (testing "Mapped to different layers"
      (is (= (mapper/map-entities->layers entities-multiple-layers multiple-layers)
-            [{:rev 1 :entity "Top/"}
-             {:rev 2 :entity "Bottom/"}]))))
+            [{:rev 1 :entity "Top"}
+             {:rev 2 :entity "infrastructure"}]))))
 
 ;; Filter out any entity that doesn't match the given layer structure.
 ;; Most of the time this is probably what we want.
 (deftest unmapped-entities-are-ignored
   (is (= (mapper/map-entities->layers entities-multiple-layers top-level-layer)
-         [{:rev 1 :entity "Top/"}])))
+         [{:rev 1 :entity "T"}])))
