1e54670c25f51ecef5c179ecd7ed1e60943c0a44
Adam Tornhill <adam@adamtornhill.com>
2015-04-06 10:03:10 +0200

    Tests the happy day scenario for code age analysis

--- test/code_maat/analysis/code_age_test.clj
+++ test/code_maat/analysis/code_age_test.clj
@@ -10,19 +10,22 @@
             [code-maat.dataset.dataset :as ds])
   (:use clojure.test))
 
-(def ^:const vcs [{:entity "A" :rev 1 :date "2014-12-25"}
-                  {:entity "B" :rev 1 :date "2014-12-31"}
-                  {:entity "A" :rev 2 :date "2015-02-28"}
-                  {:entity "A" :rev 3 :date "2015-04-05"}])
+(def ^:const vcs [{:entity "A" :rev 1 :date "2013-12-25"}
+                  {:entity "B" :rev 1 :date "2013-12-31"}
+                  {:entity "A" :rev 2 :date "2014-02-28"}
+                  {:entity "A" :rev 3 :date "2014-04-05"}])
 (def ^:const vcsd (incanter/to-dataset vcs))
 
-(def ^:const no-options {})
+;;; To make the tests deterministic we need to specify what
+;;; "now" really means.
+(def ^:const fixed-date-options
+  {:age-time-now "2014-04-06"})
 
 (defn- as-age-ds
   [result]
   (ds/-dataset [:entity :age-months] result))
 
 (deftest calculates-age-by-last-modification-date
-  (is (= (analysis/by-age vcsd no-options)
+  (is (= (analysis/by-age vcsd fixed-date-options)
          (as-age-ds [["A" 0] ["B" 3]]))))
          
