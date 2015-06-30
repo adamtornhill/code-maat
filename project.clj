b4a255c805d4c48b9a201129308e32363098d368
Adam Tornhill <adam@adamtornhill.com>
2015-06-30 07:33:26 +0200

    Updated Instaparse to 1.4.1

--- project.clj
+++ project.clj
@@ -6,8 +6,7 @@
 (defproject code-maat "0.9.1-SNAPSHOT"
   :description "A toolset to mine and analyze version control data"
   :url "http://www.adamtornhill.com/code/codemaat.htm"
-  :license {:name "GNU General Public License v3.0"
-            :url "http://www.gnu.org/licenses/gpl.html"}
+  :license {:name "GNU General Public License v3.0" :url "http://www.gnu.org/licenses/gpl.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.zip "0.1.1"]
 		 [incanter/incanter-core "1.5.6"]
@@ -16,7 +15,7 @@
                  [clj-time "0.9.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/math.combinatorics "0.1.1"]
-                 [instaparse "1.4.0"]]
+                 [instaparse "1.4.1"]]
   :main code-maat.cmd-line
   :aot [code-maat.cmd-line]
   :jvm-opts ["-Xmx4g" "-Djava.awt.headless=true" "-Xss512M"])
